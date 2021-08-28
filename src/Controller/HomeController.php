1e0bfab3e54e17a3b2f214376916819829db0da3
monoranjan.das <monoranjan.das@covianalytics.com>
2021-08-28 20:49:56 +0600

    setup beanstalk in symfony-with-docker core project

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,51 @@
 
 namespace App\Controller;
 
+use Pheanstalk\Pheanstalk;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends  AbstractController
 {
 
+    /**
+     * @return Response
+     * @Route("/", name="home")
+     */
+    public function Hello()
+    {
+//        return new Response("Hello World!");
+        return $this->render('welcome.html.twig');
+    }
+
+    /**
+     * @Route("queuetest", name="queuetest")
+     */
+    public function FunctionName(): Response
+    {
+        $pheanstalk = Pheanstalk::create('beanstalkd');
+
+        $data = [
+            'name' => 'mono',
+            'designation' => 'junior sofwere developer',
+            'company' => 'abc private LTD'
+        ];
+
+        // Queue a Job
+        $pheanstalk
+            ->useTube('testtube')
+            ->put(
+                json_encode([
+                            'headers'=>['type'=>'mail'],
+                            'body' => serialize(['data'=>$data])
+                            ],JSON_PRETTY_PRINT),  // encode data in payload
+                Pheanstalk::DEFAULT_PRIORITY,     // default priority
+                30, // delay by 30s
+                60  // beanstalk will retry job after 60s
+            );
+        return new Response('tested');
+    }
+
+        // return $this->render('$0.html.twig', []);
 }
